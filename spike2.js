04ffca346408ef0aeacb8ce95d1f4984f8399350
mxcul1 <30893516+mxcul1@users.noreply.github.com>
2017-09-03 20:43:36 +1000

    Added spike 1 and 2
    changed to naming conventions, added spike 2 to handle updating long/short inputs

---
+++ spike2.js
@@ -0,0 +1,58 @@
+//Initiliase varaibles for server use
+const express = require('express');
+const app = express();
+const admin = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json");
+var http = require('http').Server(app);
+var router = express.Router();
+var startTime = 0;
+
+//Initialise database connection
+admin.initializeApp({
+  databaseURL: "https://project1-1d4a2.firebaseio.com/",
+  credential: admin.credential.cert(serviceAccount)
+});
+
+//initialise database variables
+var db = admin.database();
+var timedata = db.ref("serverData2");
+var alldata = db.ref("serverData3");
+var startingTime = db.ref("serverData2").child("startTime");
+var endingTime = db.ref("serverData2").child("endTime");
+
+
+//allow errors to be printed to console
+alldata.on("value", function(snapshot) {
+	console.log(snapshot.val())
+	}, function(errorObject){
+	console.log("Failed" + errorObject.code)
+});
+
+timedata.on("value", function(snapshot) {
+	console.log(snapshot.val())
+	motionTime = endingTime - startingTime
+	//now check to see whether value was short or long motion, using milliseconds
+	if((motionTime > 5000)) {
+		console.log("A long motion has been detected.");
+		timedata.transaction(function(longMotion){
+			return(longMotion || 0) + 1
+		});
+	}
+	else if((motionTime < 5000)&&(motionTime > 0)) {
+		console.log("A short motion has been detected.");
+			timeData.transaction(function(shortMotion){
+				return(shortMotion || 0) - 1
+			});
+	
+	};
+});
+router.use(function(req, res, next){
+	next()
+});
+
+
+app.use("/", router)
+
+http.listen(8080, function(){
+	console.log("Live at port 8080")
+});
