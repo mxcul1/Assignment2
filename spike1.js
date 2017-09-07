177f136c76604d30bb3bf112de607e4547f43104
Luke Cimorelli <luke.cimo@gmail.com>
2017-09-07 18:49:09 +1000

    Rename js files

--- spike1.js
+++ spike1.js
@@ -7,10 +7,6 @@ var http = require('http').Server(app);
 var router = express.Router();
 var startTime = 0;
 
-//initialise board
-var five = require('johnny-five');
-var board = new five.Board();
-
 //Initialise database connection
 admin.initializeApp({
   databaseURL: "https://project1-1d4a2.firebaseio.com/",
@@ -19,8 +15,12 @@ admin.initializeApp({
 
 //initialise database variables
 var db = admin.database();
-var alldata = db.ref("serverData2");
-
+var timedata = db.ref("serverData2");
+var alldata = db.ref("serverData3");
+var startingTime = db.ref("serverData2").child("startTime");
+var endingTime = db.ref("serverData2").child("endTime");
+var s = db.ref("serverData3").child("shortMotion");
+var l = db.ref("serverData3").child("longMotion");
 
 //allow errors to be printed to console
 alldata.on("value", function(snapshot) {
@@ -29,48 +29,33 @@ alldata.on("value", function(snapshot) {
 	console.log("Failed" + errorObject.code)
 });
 
-
-//get board ready, and observing
-board.on("ready", function(){
-	var pin = 7;
-	var motion = new five.Motion(pin);
-
-  // "calibrated" occurs once, at the beginning of a session,
-  motion.on("calibrated", function() {
-    console.log("calibrated");
-  });
-
-	//initialise start time
-	motion.on("motionstart", function() {
-		startDate = new Date();
-		startTime = startDate.getTime();
-		console.log("Motion has started at" + startTime)
-		alldata.update({startTime})
-	});
-
-	//initialise end time
-	motion.on("motionend", function() {
-		endDate = new Date();
-		endTime = endDate.getTime();
-		console.log("Motion has ended at" + endTime);
-		alldata.update({endTime})
-		alldata.update({timeStamp:endTime})
-	});
+timedata.on("value", function(snapshot) {
+	data = snapshot.val();
+	sTime = data.startTime
+	eTime = data.endTime
+	motionTime = eTime - sTime
+	//now check to see whether value was short or long motion, using milliseconds
+	if((motionTime > 5000)) {
+		console.log("A long motion has been detected.");
+		l.transaction(function(longMotion){
+			return(longMotion || 0) + 1
+		});
+	}
+	else if((motionTime < 5000)&&(motionTime > 0)) {
+		console.log("A short motion has been detected.");
+			s.transaction(function(shortMotion){
+				return(shortMotion || 0) + 1
+			});
+	
+	};
 });
-
 router.use(function(req, res, next){
 	next()
-});
-
-router.get("/", function(req, res){
-	res.sendFile("index.html", {root: __dirname});
 });
 
 
 app.use("/", router)
 
-http.listen(8000, function(){
-	console.log("Live at port 8000")
+http.listen(8080, function(){
+	console.log("Live at port 8080")
 });
-
-
