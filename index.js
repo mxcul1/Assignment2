3271af0aa23061934628a27adbcaa0d9467aef9a
mxcul1 <30893516+mxcul1@users.noreply.github.com>
2017-09-10 12:39:46 +1000

    Updated email firebase function
    Emails now contain accurate information, that is formatted

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ exports.sendEmail = functions.database.ref('serverData2/timeStamp')
       var mailOptions = {
         from: 'fit3140.team2424@gmail.com',
         to: 'fit3140.team2424@gmail.com',
-        subject: 'Sending Email using Node.js',
+        subject: 'Long motion detected: firebase',
         text: 'Dear user,' + "\n" + "\n" + 'A long motion was detected by firebase.' + "\n" + "\n" + "Regards, " + "\n" + "Management team."
       };
       transporter.sendMail(mailOptions, function(error, info){
@@ -47,7 +47,7 @@ exports.sendEmailMotionChange = functions.database.ref('serverData2/timeStamp')
         var mailOptions = {
           from: 'fit3140.team2424@gmail.com',
           to: 'fit3140.team2424@gmail.com',
-          subject: 'Sending Email using Node.js',
+          subject: 'Motion change detected: firebase',
           text:'Dear user,' + "\n" + "\n" + 'A change in motion was detected by firebase. Here are the changes: ' + "\n" + "\n" + 'Number of long motions are: ' + long + "\n" + "Number of short motions are: " + short + "\n" + "\n" + "Regards, " + "\n" + "Management team."
         };
         transporter.sendMail(mailOptions, function(error, info){
